bcfe2aa64d599ac1ca91132148f724d714790b40
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-10-09 16:38:36 +0100

    Create a new assignment per role when week created

--- app/models/week.rb
+++ app/models/week.rb
@@ -1,3 +1,13 @@
 class Week < ActiveRecord::Base
+  after_create :create_assignments
+  has_many :assignments
+
+ROLES = %w(oncall_weekday oncall_weekend primarydev supplementaldev infrastructuredev)
+
+	def create_assignments
+		ROLES.each do |role|
+			assignments.create(role: role)
+		end
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :people
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,7 +13,10 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
+
+  resources :assignments do
     resources :weeks
+  end
 
   # Example resource route with options:
   #   resources :products do
