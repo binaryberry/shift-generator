e40941113e6ef5594d996e03a8bc71269cabb9ba
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2015-12-16 12:26:08 +0000

    WIP

--- spec/unit_tests/scheduler_spec.rb
+++ spec/unit_tests/scheduler_spec.rb
@@ -11,17 +11,24 @@ describe Scheduler do
   let!(:assignment_one) { create(:primary_dev_assignment, week: previous_week, person: bob) }
   let!(:assignment_two) { create(:supplemental_dev_assignment, week: previous_week, person: alex) }
 
-  context "when creating a new week" do
   	it "knows about the assignments of the previous week" do
       expect(scheduler.history(new_week)).to match_array [assignment_one, assignment_two]
   	end
 
-    it "knows about the assignments of the previous 2 weeks" do
-      pre_previous_week = Week.create!(start_date: Date.new(2015,11,13))
-      assignment_three = Assignment.create(week: pre_previous_week, person: bob, role: "primary_dev")
-      assignment_four = Assignment.create(week: pre_previous_week, person: alex, role: "supplemental_dev")
+    it "knows about the assignments of the previous 3 weeks" do
+      pre_previous_week = create(:week, start_date: Date.new(2015,11,13))
+      assignment_three = create(:primary_dev_assignment, week: pre_previous_week, person: bob)
+      assignment_four = create(:supplemental_dev_assignment, week: pre_previous_week, person: alex)
 
       expect(scheduler.history(new_week, 2)).to match_array [assignment_one, assignment_two, assignment_three, assignment_four]
+    end
+
+#use shuffle to randomise things. Stub it to remove randomness. One method per rule, each returns a list of valid devs. note to self: could be a good indicator of too many rules and not enough devs. 
+    context "when creating a new week with valid assignments" do
+      it "ensures there are 4 weeks between 2 shifts" do
+        a_week_1 = scheduler.produce_assignments(new_week)
+        expect(a_week.rule_1).to match_array()
+      end
     end
   end
 end
