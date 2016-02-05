20e96aa298fa2b9433507b32ebadbb6d5954907a
Tatiana Soukiassian <tatiana.soukiassian@digital.cabinet-office.gov.uk>
2016-02-05 17:31:23 +0000

    Correct bug that presented an empty column

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -3,15 +3,17 @@ class WeeksController < ApplicationController
 
   def index
     @week = Week.new(start_date: Week.default_start_date)
+
     Week.roles.each do |role|
       @week.assignments.build(role: role)
     end
   end
 
   def create
-    @week = Week.new(week_params)
+    @week = Week.create!(week_params)
     scheduler = Scheduler.new(@week)
-    Week.roles.each{|role| scheduler.assign(role)}
+    Week.roles.each {|role| scheduler.assign(role)}
+
     if @week.persisted?
       redirect_to weeks_path
     else
